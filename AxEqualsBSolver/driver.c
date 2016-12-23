69ed4d8df1237c16531557f4377d993e6b48a6de
rsubramonian <rsubramonian@nerdwallet.com>
2016-12-22 16:13:43 -0800

    realized that solver was over-writing b

--- AxEqualsBSolver/driver.c
+++ AxEqualsBSolver/driver.c
@@ -10,6 +10,86 @@
 #include "macros.h"
 #include "positive_solver.h"
 /* some utilities */
+static void  free_matrix(
+      double **A,
+      int n
+      )
+{
+  if ( A != NULL ) { 
+    for ( int i = 0; i < n; i++ ) { 
+      free_if_non_null(A[i]);
+    }
+  }
+  free_if_non_null(A);
+}
+static void multiply_matrix_vector(
+    double **A, 
+    double *x,
+    int n,
+    double *b
+    )
+{
+  for ( int i = 0; i < n; i++ ) { 
+    double sum = 0;
+    for ( int j = 0; j < n; j++ ) { 
+      sum += A[i][j] * x[j];
+    }
+    b[i] = sum;
+  }
+}
+static int 
+alloc_matrix(
+    double ***ptr_X, 
+    int n
+    )
+{
+  int status = 0;
+  double **X = NULL;
+  *ptr_X = NULL;
+  X = (double **) malloc(n * sizeof(double*));
+  return_if_malloc_failed(X);
+  for ( int i = 0; i < n; i++ ) { X[i] = NULL; }
+  for ( int i = 0; i < n; i++ ) { 
+    X[i] = (double *) malloc(n * sizeof(double));
+    return_if_malloc_failed(X[i]);
+  }
+  *ptr_X = X;
+BYE:
+  return status;
+}
+static void 
+multiply(
+    double **A,
+    double **B,
+    double **C,
+    int n
+    )
+{
+  for ( int i = 0; i < n; i++ ) { 
+    for ( int j = 0; j < n; j++ ) { 
+      double sum = 0;
+      for ( int k = 0; k < n; k++ ) { 
+        sum += A[i][k] * B[k][j];
+      }
+      C[i][j] = sum;
+    }
+  }
+}
+
+static void 
+transpose(
+    double **A,
+    double **B,
+    int n
+    )
+{
+  for ( int i = 0; i < n; i++ ) { 
+    for ( int j = 0; j < n; j++ ) { 
+      B[i][j] = A[j][i];
+    }
+  }
+}
+
 static void
 print_input(
     double **A, 
@@ -63,11 +143,14 @@ main(
 {
   int status = 0;
   double **A = NULL;
+  double **AT = NULL; // A transpose
+  double **AAT = NULL; // A times A transpose
   double **Aprime = NULL;
   double *x_expected = NULL;
   double *x = NULL; // allocated by positive_solver
   double *b = NULL;
   double *bprime = NULL;
+  double *b_solver = NULL; // since solver messes up original b
   int n = 0;
   srand48(RDTSC());
   fprintf(stderr, "Usage is ./driver <n> \n");
@@ -75,18 +158,14 @@ main(
     case 2 : n = atoi(argv[1]); break;
     default : go_BYE(-1); break;
   }
+  status = alloc_matrix(&A, n); cBYE(status);
+  status = alloc_matrix(&AT, n); cBYE(status);
+  status = alloc_matrix(&AAT, n); cBYE(status);
 
-  A = (double **) malloc(n * sizeof(double*));
-  return_if_malloc_failed(A);
-  for ( int i = 0; i < n; i++ ) { A[i] = NULL; }
-  for ( int i = 0; i < n; i++ ) { 
-    A[i] = (double *) malloc(n * sizeof(double));
-    return_if_malloc_failed(A[i]);
-  }
   /* Note that A is symmetric. We define top half */
   for ( int i = 0; i < n; i++ )  {
     for ( int j = i; j < n; j++ ) {
-      A[i][j] = llabs(lrand48() % 16) + 1;
+      A[i][j] = llabs(lrand48() % 4) + 1;
     }
   }
   /* Now we copy top right half to bottom left half */
@@ -95,6 +174,10 @@ main(
       A[i][j] = A[j][i];
     }
   }
+  transpose(A, AT, n);
+  multiply(A, AT, AAT, n);
+  b_solver = (double *) malloc(n * sizeof(double));
+  return_if_malloc_failed(b_solver);
   bprime = (double *) malloc(n * sizeof(double));
   return_if_malloc_failed(bprime);
   b = (double *) malloc(n * sizeof(double));
@@ -105,14 +188,7 @@ main(
   for ( int i = 0; i < n; i++ )  {
     x_expected[i] = (lrand48() % 16) - 16/2;
   }
-  // Compute b
-  for ( int i = 0; i < n; i++ ) { 
-    double sum = 0;
-    for ( int j = 0; j < n; j++ ) { 
-      sum += A[i][j] * x_expected[j];
-    }
-    b[i] = sum;
-  }
+  multiply_matrix_vector(AAT, x_expected, n, b);
   /* The solver does not want a full matrix. So, we must convert A 
      to Aprime, which is allocated and initialized differently. */
   Aprime = (double **) malloc(n * sizeof(double*));
@@ -122,31 +198,31 @@ main(
     Aprime[i] = (double *) malloc((n-i) * sizeof(double));
     return_if_malloc_failed(Aprime[i]);
   }
+  //---------------------------
   for ( int i = 0; i < n; i++ ) { 
     for ( int j = 0; j < n-i; j++ ) { 
-      Aprime[i][j] = A[i][j+i];
+      Aprime[i][j] = AAT[i][j+i];
     }
   }
   //-----------------
-  print_input(A, Aprime, x_expected, b, n);
-  x = positive_solver(Aprime, b, n);
+  for ( int i = 0; i < n; i++ ) { 
+    b_solver[i] = b[i];
+  }
+  print_input(AAT, Aprime, x_expected, b_solver, n);
+  x = positive_solver(Aprime, b_solver, n);
 
   fprintf(stderr, "x from solver is [ ");
   for (int i=0; i < n; i++) {
     fprintf(stderr, " %lf ", x[i]);
   }
   fprintf(stderr, "].\nChecking commences \n");
-  // Compute b
-  for ( int i = 0; i < n; i++ ) { 
-    double sum = 0;
-    for ( int j = 0; j < n; j++ ) { 
-      sum += A[i][j] * x[j];
-    }
-    bprime[i] = sum;
-  }
+  // Compute bprime
+  multiply_matrix_vector(AAT, x, n, bprime);
   bool error = false;
+  fprintf(stderr, "x(good) x(computed) b(good), b(solver) \n");
   for (int i=0; i < n; i++) {
-    fprintf(stderr, " %lf %lf ", bprime[i], b[i]);
+    fprintf(stderr, " %lf %lf %lf %lf ", 
+        x_expected[i], x[i], b[i], bprime[i]);
     if ( abs(bprime[i] - b[i]) < 0.001 ) { 
       fprintf(stderr, " MATCH \n");
     }
@@ -162,22 +238,20 @@ main(
     fprintf(stderr, "SUCCESS\n");
   }
 BYE:
+  free_matrix(A, n);
+  free_matrix(AT, n);
+  free_matrix(AAT, n);
   if ( Aprime != NULL ) { 
     for ( int i = 0; i < n; i++ ) { 
       free_if_non_null(Aprime[i]);
     }
   }
   free_if_non_null(Aprime);
-  if ( A != NULL ) { 
-    for ( int i = 0; i < n; i++ ) { 
-      free_if_non_null(A[i]);
-    }
-  }
-  free_if_non_null(A);
   free_if_non_null(x);
   free_if_non_null(x_expected);
   free_if_non_null(b);
   free_if_non_null(bprime);
+  free_if_non_null(b_solver);
   return status;
 }
 /*
